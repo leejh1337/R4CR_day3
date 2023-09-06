45e04cdb66dd24fda0fb9769bfc12f69f886a9f5
leejh1337 <leejh7745@gmail.com>
2023-09-06 06:47:47 +0000

    pkg

--- install.R
+++
@@ -1,5 +0,0 @@
-install.packages(
-  c('palmerpenguins', 'rmarkdown', 'tidyverse', 'knitr', 'reactable', 'DT', # quarto
-  'shiny', 'shinydashboard', 'bslib', # shiny
-  'usethis', 'devtools', 'roxygen2', 'remotes') # R packages
-)
--- shinydashboard.R
+++
@@ -1,3 +0,0 @@
-remotes::install_github('jinseob2kim/jsmodule') # Github package
-
-install.packages('shiny') # CRAN package 
